6c26cf654ccb6b60b43ff82be5dcb64a2f696633
brosie224 <barosengarten@gmail.com>
2019-09-10 03:18:08 -0400

    Added User model and Sessions controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,25 @@
 class ApplicationController < ActionController::Base
-end
+    protect_from_forgery
+  
+    helper_method :current_user
+  
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
+  
+    def admin_required
+      if current_user.email != "barosengarten@gmail.com"
+        flash[:alert] = "You must be an admin to perform this action."
+        redirect_to root_path
+      end
+    end
+  
+    def commissioner_required
+      commissioners = ["barosengarten@gmail.com"]
+      if !commissioners.include? current_user.email
+        flash[:alert] = "You must be a commissioner to perform this action."
+        redirect_to root_path
+      end
+    end
+      
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,24 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root '/'
+
+  get '/commissioner/login' => 'sessions#new'
+  post '/commissioner/login' => 'sessions#create'
+  get '/commissioner/logout' => 'sessions#destroy'
+  
+  # namespace 'commissioner' do
+  #   transactions: new, create, edit, update, destroy (index for all to see)
+  #   newsletter
+  # end
+
+  # namespace 'admin' do
+  #   resources :players, only: [:new, :edit] # admin only in controller
+  #   whatever else
+  # end
+
+  # namespace 'api' do
+  #   namespace 'v1' do
+  #     resources :players, only: [:index, :show, :create, :update, :delete]
+  #   end
+  # end
+
 end
