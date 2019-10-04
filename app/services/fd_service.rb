73fff6a2d6c6fdfaea6d67728fbd9b1cfcaf3b47
brosie224 <barosengarten@gmail.com>
2019-10-04 00:29:36 -0400

    Added methods to update tlfl team and team dst data

--- app/services/fd_service.rb
+++ app/services/fd_service.rb
@@ -78,9 +78,6 @@ class FdService
         @active_players = actives.select { |player| %w(QB RB FB WR TE K).include? player["Position"] }
     end
 
-
-    # -- NEW SEASON UPDATES --
-
     def create_new_players
         get_player_data
         @active_players = @active_players.sort_by { |player| player["LastName"] }
@@ -96,6 +93,8 @@ class FdService
             end
         end
     end
+
+    # -- NEW SEASON UPDATES --
 
     # Updates NFL FAs (Creates and Deletes to show correct available players)
     def update_available_players
@@ -151,16 +150,37 @@ class FdService
           end
       end
     
-    def update_teams_dst_data
-        # no find_or_create, just matchup IDs and save the data
-        # copy the create method with if statement instead
-        # If new team, check abbreviation compared to what cbs and pfb uses
+    def update_team_dst_data
+        # If there's a new/relocated team, check abbreviation compared to what cbs and pfb uses
+        teams_resp = Faraday.get 'https://api.fantasydata.net/api/nfl/fantasy/json/Teams' do |req|
+            req.params['key'] = ENV['FANTASY_DATA_KEY']
+        end
+        teams_json = JSON.parse(teams_resp.body)
+        teams_json.each do |team|
+            TeamDst.all.each do |dst|
+                if dst.fd_id == team["TeamID"]
+                    dst.update(city: team["City"], nickname: team["Name"], nfl_abbrev: team["Key"], bye_week: team["ByeWeek"], 
+                        logo: team["WikipediaLogoUrl"], word_mark: team["WikipediaWordMarkUrl"], fd_player_id: team["PlayerID"])
+                end
+            end
+        end
     end
 
     def update_tlfl_team_data
-        # no find_or_create, just matchup IDs and save the data
-        # copy the create method with if statement instead
-        # If new team, check abbreviation compared to what cbs and pfb uses
+        # If there's a new/relocated team, check abbreviation compared to what cbs and pfb uses
+        teams_resp = Faraday.get 'https://api.fantasydata.net/api/nfl/fantasy/json/Teams' do |req|
+            req.params['key'] = ENV['FANTASY_DATA_KEY']
+        end
+        teams_json = JSON.parse(teams_resp.body)
+        teams_json.each do |team|
+            TlflTeam.all.each do |tlfl_team|
+                if tlfl_team.fd_id == team["TeamID"]
+                    tlfl_team.update(city: team["City"], nickname: team["Name"], abbreviation: team["Key"], conference: team["Conference"], division: team["Division"], 
+                        bye_week: team["ByeWeek"], logo: team["WikipediaLogoUrl"], word_mark: team["WikipediaWordMarkUrl"], primary_color: team["PrimaryColor"], 
+                        secondary_color: team["SecondaryColor"], tertiary_color: team["TertiaryColor"], quaternary_color: team["QuaternaryColor"])
+                end
+            end
+        end
     end
 
 end
