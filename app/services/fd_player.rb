1acb1f90adbe449a174bca67161d61f56a589460
brosie224 <barosengarten@gmail.com>
2019-12-13 15:17:53 -0500

    Created FdService

--- app/services/fd_player.rb
+++ app/services/fd_player.rb
@@ -1,28 +1,9 @@
 require 'open-uri'
-
 class FdPlayer
 
-# -- IN-SEASON METHODS -- 
-
-    def current_timeframe
-        time_resp = Faraday.get 'https://api.fantasydata.net/api/nfl/fantasy/json/Timeframes/current' do |req|
-            req.params['key'] = ENV['FANTASY_DATA_KEY']
-        end
-        time_json = JSON.parse(time_resp.body)
-        @current_season = time_json["Season"] # eg 2019
-        @current_week = time_json["Week"]
-        @current_api_season = time_json["ApiSeason"] # eg 2019REG
-        @current_season_type = time_json["SeasonType"] # (1=Regular Season, 2=Preseason, 3=Postseason, 4=Offseason)
-        # find out when current week flips and run PlayerGame and Projections accordingly
-    end
+# -- WEEKLY STATS -- 
 
     def get_player_games
-        # current_timeframe
-        @current_api_season = "2018REG" # delete once timeframe running
-        @current_season = 2018 # delete once timeframe running
-        @current_week = 3 # delete once timeframe running
-        @current_season_type = 1 # delete once timeframe running
-        # -----------------------------------------------------------------
         stats_resp = Faraday.get "https://api.fantasydata.net/api/nfl/fantasy/json/PlayerGameStatsByWeek/#{@current_api_season}/#{@current_week}" do |req|
             req.params['key'] = ENV['FANTASY_DATA_KEY']
         end
@@ -35,7 +16,8 @@ class FdPlayer
     end
 
     def create_skill_player_games
-        # if @current_season_type == 1
+        FdService.new.current_timeframe
+        if @current_season_type == 1
             week_injury_status
             get_player_games
             @tlfl_skill_players.each do |tlfl_player|
@@ -109,7 +91,7 @@ class FdPlayer
                     )
                 end
             end
-        # end
+        end
     end
 
     def week_injury_status 
@@ -153,6 +135,8 @@ class FdPlayer
             end
         end
     end
+
+    # -- IN-SEASON METHODS --
 
     # Creates and Deletes players to show only active NFL players
     def update_available_players
---
+++ app/services/fd_service.rb
@@ -0,0 +1,20 @@
+class FdService
+
+    def current_timeframe
+        # time_resp = Faraday.get 'https://api.fantasydata.net/api/nfl/fantasy/json/Timeframes/current' do |req|
+        #     req.params['key'] = ENV['FANTASY_DATA_KEY']
+        # end
+        # time_json = JSON.parse(time_resp.body)
+        # @current_season = time_json["Season"] # eg 2019
+        # @current_week = time_json["Week"]
+        # @current_api_season = time_json["ApiSeason"] # eg 2019REG
+        # @current_season_type = time_json["SeasonType"] # (1=Regular Season, 2=Preseason, 3=Postseason, 4=Offseason)
+        # # find out when current week flips and run PlayerGame and Projections accordingly
+
+        @current_api_season = "2018REG" # delete once timeframe running
+        @current_season = 2018 # delete once timeframe running
+        @current_week = 3 # delete once timeframe running
+        @current_season_type = 1 # delete once timeframe running
+    end
+
+end
