39b94a9842753cea1645ff3c1e92700d67ec9bbc
brosie224 <barosengarten@gmail.com>
2019-12-18 00:21:44 -0500

    Refactored TlflTeam week_pts and season_pts

--- app/models/tlfl_team.rb
+++ app/models/tlfl_team.rb
@@ -5,36 +5,15 @@ class TlflTeam < ApplicationRecord
     has_many :draft_picks
 
     def week_pts(season, season_type = 1, week)
-        player_games = PlayerGame.where(tlfl_team_id: self.id, season: season, season_type: season_type, week: week).sort_position
-        all_player_pts = []
-        player_games.each do |player_game|
-            puts "#{player_game.position} #{player_game.player_name}: #{player_game.tlfl_pts}"
-            player_pts = {}
-            player_pts[:tlfl_pts] = player_game.tlfl_pts
-            all_player_pts << player_pts
-        end
+        player_games = PlayerGame.where(tlfl_team_id: self.id, season: season, season_type: season_type, week: week)
         dst_game = TeamDstGame.find_by(tlfl_team_id: self.id, season: season, season_type: season_type, week: week)
-        puts "#{dst_game.team_name}: #{dst_game.tlfl_pts}"
-        total = all_player_pts.inject(0) {|sum, hash| sum + hash[:tlfl_pts]} + dst_game.tlfl_pts
-        puts "Total: #{total}"
+        player_games.sum(&:tlfl_pts) + dst_game.tlfl_pts
     end
 
     def season_pts(season, season_type = 1)
         player_games = PlayerGame.where(tlfl_team_id: self.id, season: season, season_type: season_type)
-        all_player_pts = []
-        player_games.each do |player_game|
-            player_pts = {}
-            player_pts[:tlfl_pts] = player_game.tlfl_pts
-            all_player_pts << player_pts
-        end
         dst_games = TeamDstGame.where(tlfl_team_id: self.id, season: season, season_type: season_type)
-        all_dst_pts = []
-        dst_games.each do |dst_game|
-            dst_pts = {}
-            dst_pts[:tlfl_pts] = dst_game.tlfl_pts
-            all_dst_pts << dst_pts
-        end
-        all_player_pts.inject(0) {|sum, hash| sum + hash[:tlfl_pts]} + all_dst_pts.inject(0) {|sum, hash| sum + hash[:tlfl_pts]}
+        player_games.sum(&:tlfl_pts) + dst_games.sum(&:tlfl_pts)
     end
     
     def full_name
